056d354e840b2f6a5c06f7d124dd957579dceb35
nicolet2132 <nicole.nt24@gmail.com>
2013-12-02 20:40:47 -0700

    revert back to old client commands

--- TISCaP_Server_Project/src/ClientCommand.java
+++ TISCaP_Server_Project/src/ClientCommand.java
@@ -1,66 +1,108 @@
 /**
  * Handler for incoming client commands
  * 
- * Parses incoming strings and returns usable commands, arguments, and data 
+ * Parses incoming strings and returns usable commands, arguments, and data
  * 
  * @authors Kyle Swanson, Nicole Thomas
- *
+ * 
  */
 
 public class ClientCommand {
-	
-	// instance variables
-	protected String command;
-	protected String arg = "";
-	protected String data = "";
+	public String command;
+	public String arg = "";
+	public String data = "";
 
-	// parses client input
-	protected static ClientCommand parse(String input) {
+	public static ClientCommand parse(String input) {
+		boolean debug = false;
+
+		if (debug) {
+			System.out.println("Raw Input: " + input);
+		}
 
 		ClientCommand out = new ClientCommand();
 
-		// check for empy command
 		if (input.length() == 0) {
 			out.command = "";
 		} else {
-			
-			// split up the input so we can work with it
-			String[] args = input.split(" ");
 
-			// first word should be command
+			int s = input.indexOf("\\r\\n");
+			System.out.println("before " + s);
+			if (s == -1) {
+				s = input.length();
+			}
+			System.out.println("after " + s);
+			String before = input.substring(0, s);
+			if (s < input.length()) {
+				out.data = input.substring(s + 4);
+				System.out.println(out.data);
+			}
+
+			String[] args = before.split(" ");
 			out.command = args[0].substring(1).toLowerCase();
 
-			// handle commands with arguments or data
 			if (args.length > 1) {
-				
 				out.arg = args[1];
-				
-				// public message
-				if (out.command.equals("public") && args[1].equals("\\r\\n")) {
-					// form the client message from remaining arguments
-					for (int i = 2; i < args.length; i++) {
-						out.data = out.data.concat(args[i] + " ");
-					}
-					// remove any trailing spaces
-					out.data = out.data.trim();
-				}
-				
-				// private message
-				if (out.command.equals("private") && args.length > 2) {
-					// pre-error checking
-					if (!args[2].equals("\\r\\n")) {
-						out.arg = args[1] + " " + args[2];
-					} else {
-					// form the client message from remaining arguments
-						for (int i = 3; i < args.length; i++) {
-							out.data = out.data.concat(args[i] + " ");
-						}
-					}
-					// remove any trailing spaces
-					out.data = out.data.trim();
-				}
 			}
 		}
+
 		return out;
 	}
 }
+
+// public class ClientCommand {
+//
+// // instance variables
+// protected String command;
+// protected String arg = "";
+// protected String data = "";
+//
+// // parses client input
+// protected static ClientCommand parse(String input) {
+//
+// ClientCommand out = new ClientCommand();
+//
+// // check for empy command
+// if (input.length() == 0) {
+// out.command = "";
+// } else {
+//
+// // split up the input so we can work with it
+// String[] args = input.split(" ");
+//
+// // first word should be command
+// out.command = args[0].substring(1).toLowerCase();
+//
+// // handle commands with arguments or data
+// if (args.length > 1) {
+//
+// out.arg = args[1];
+//
+// // public message
+// if (out.command.equals("public") && args[1].equals("\\r\\n")) {
+// // form the client message from remaining arguments
+// for (int i = 2; i < args.length; i++) {
+// out.data = out.data.concat(args[i] + " ");
+// }
+// // remove any trailing spaces
+// out.data = out.data.trim();
+// }
+//
+// // private message
+// if (out.command.equals("private") && args.length > 2) {
+// // pre-error checking
+// if (!args[2].equals("\\r\\n")) {
+// out.arg = args[1] + " " + args[2];
+// } else {
+// // form the client message from remaining arguments
+// for (int i = 3; i < args.length; i++) {
+// out.data = out.data.concat(args[i] + " ");
+// }
+// }
+// // remove any trailing spaces
+// out.data = out.data.trim();
+// }
+// }
+// }
+// return out;
+// }
+// }
--- TISCaP_Server_Project/src/Connection.java
+++ TISCaP_Server_Project/src/Connection.java
@@ -19,10 +19,10 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Connection implements Runnable {
-	
-	// static 
+
+	// static
 	private static final int BUFFER_SIZE = 2048;
-	
+
 	// instance variables
 	private Socket client;
 	private List<Connection> clients;
@@ -46,6 +46,7 @@ public class Connection implements Runnable {
 	 * The only global method from this class.
 	 */
 	public void run() {
+		// LOGIC UP IN HERE!
 		byte[] buffer = new byte[BUFFER_SIZE];
 		fromClient = null;
 		toClient = null;
@@ -74,7 +75,8 @@ public class Connection implements Runnable {
 
 				ClientCommand cc = ClientCommand.parse(input);
 
-				// check if user has already logged in
+				System.out.println("Requested Command: " + cc.command);
+
 				if (active == true && cc.command.equals("login")) {
 					writeError("You have already logged in.");
 				}
@@ -82,7 +84,7 @@ public class Connection implements Runnable {
 				// Log the user in.
 				while (active == false) {
 					if (!cc.command.equals("login") || cc.arg.isEmpty() || cc.command.equals("")) {
-						writeBadSyntax("Login command is '/login username \\r\\n\'");
+						writeBadSyntax("Login command is '/login username \\r\\n'");
 						return;
 					} else {
 						if (users.contains(cc.arg)) {
@@ -91,8 +93,6 @@ public class Connection implements Runnable {
 							client.close();
 						} else if (cc.arg.length() > 16) {
 							writeError("Username must be between 1 and 16 characters");
-							// Close the connection.
-							client.close();
 						} else {
 							uname = cc.arg;
 							users.add(uname);
@@ -106,35 +106,26 @@ public class Connection implements Runnable {
 
 				// command and error handling
 				if (cc.command.equals("public")) {
-					// public messages
-					if (!cc.arg.equals("\\r\\n")) {
-						writeBadSyntax("");
-					} else {
-						publicToAllClients(cc.data, uname);
-					}
+					System.out.println("public msg:" + cc.data);
+					publicToAllClients(cc.data, uname);
 				} else if (cc.command.equals("private")) {
-					// private messages
-					String args[] = cc.arg.split(" ");
-					if (args.length > 1) {
-						String name = args[0];
-						String rtn = args[1];
-						if (name.equals("\\r\\n")) {
-							writeError("Enter a username.");
-						} else if (!rtn.equals("\\r\\n")) {
-							writeBadSyntax("Private command is '/private username \\r\\n mesage\'");
-						}
+					if (cc.arg.isEmpty()) {
+						writeError("Enter a username.");
 					} else {
+						// TODO: data not getting set...
+						System.out.println("data = " + cc.data);
+						System.out.println("arg = " + cc.arg);
 						privateToUser(cc.data, cc.arg);
 					}
 				} else if (cc.command.equals("users")) {
-					// users: check for extra arguments
+					// check for extra args: bad syntax
 					if (cc.arg.equals("")) {
 						writeUserList(users);
 					} else {
-						writeBadSyntax("/users takes no arguments");
+						writeBadSyntax("/activeusers takes no arguments");
 					}
 				} else if (cc.command.equals("close")) {
-					// close: check for extra arguments
+					// check for extra args: bad syntax
 					if (cc.arg.equals("")) {
 						writeToAllClients("Disconnected " + uname + "\r\n");
 						client.close();
@@ -144,10 +135,11 @@ public class Connection implements Runnable {
 						writeBadSyntax("/close takes no arguments");
 					}
 				} else if (cc.command.equals("login")) {
-					// handles lack of command immediately after logging in
+					// if the user logs in, but hasn't submitted a new command,
+					// do not throw an error
 					continue;
 				} else {
-					// if user enters ANYTHING else, it's not a valid command!
+					// if the user enters else, it's not a supported command!
 					writeBadSyntax("");
 				}
 			}
@@ -160,17 +152,14 @@ public class Connection implements Runnable {
 					fromClient.close();
 				if (toClient != null)
 					toClient.close();
-			} catch (Exception e) {}
+			} catch (Exception e) {
+			}
 		}
 	}
-	
-	/**
-	 * Helper methods!
-	 */
 
-	// writes the user list to client
 	private void writeUserList(List<String> u) {
 		String out = "ActiveUsers {";
+
 		synchronized (u) {
 			Iterator<String> i = u.iterator();
 			while (i.hasNext()) {
@@ -180,11 +169,12 @@ public class Connection implements Runnable {
 					out = out + ",";
 			}
 		}
+
 		out = out + "}\r\n";
+
 		writeToClient(out);
 	}
 
-	// writes a private message to a specified user
 	private void privateToUser(String input, String dest_uname) {
 		synchronized (clients) {
 			Connection dst = null;
@@ -194,21 +184,17 @@ public class Connection implements Runnable {
 					break;
 				}
 			}
-			if (dst != null && dest_uname.equals(uname)) {
-				// user is sending private message to self: allowed, but silly
-				String funny = "Why are you writing messages to yourself?\r\n";
-				dst.writeToClient("Private " + uname + "\r\n" + funny + input + "\u0004\r\n");
-			} else if (dst != null) {
-				// main private message
-				dst.writeToClient("Private " + uname + "\r\n" + input + "\u0004\r\n");
+
+			if (dst != null) {
+				dst.writeToClient("Private " + uname + "\r\n" + input + "\u0004");
 			} else {
-				// username was not found
-				writeError("User '" + dest_uname + "' not found");
+				// TODO Return UserNotFound
+				writeError("User not found");
+				System.out.println("User not found... '" + dest_uname + "'");
 			}
 		}
 	}
 
-	// writes input to all clients, such as connected and disconnected users
 	private void writeToAllClients(String input) {
 		synchronized (clients) {
 			Iterator<Connection> i = clients.iterator();
@@ -220,7 +206,6 @@ public class Connection implements Runnable {
 		}
 	}
 
-	// writes a public messages to all clients
 	private void publicToAllClients(String input, String sender_uname) {
 		synchronized (clients) {
 			Iterator<Connection> i = clients.iterator();
@@ -232,12 +217,10 @@ public class Connection implements Runnable {
 		}
 	}
 
-	// helper method for publicToAllClients
 	private void writePublicMessage(String msg, String sender_uname) {
-		writeToClient("Public " + sender_uname + "\r\n" + msg + "\u0004\r\n");
+		writeToClient("Public " + sender_uname + "\r\n" + msg + "\u0004");
 	}
 
-	// meat and potatoes - writing messages!
 	private void writeToClient(String msg) {
 		msg = "]" + msg;
 		try {
@@ -247,19 +230,233 @@ public class Connection implements Runnable {
 		}
 	}
 
-	/**
-	 * Error message methods!
-	 * @param msg
-	 */
+	// errors
 
-	// bad syntax: takes an input message if desired
 	private void writeBadSyntax(String msg) {
 		writeToClient("BadSyntax " + msg + "\r\n");
 	}
 
-	// general error: takes an input message if desired
 	private void writeError(String msg) {
 		writeToClient("Error " + msg + "\r\n");
 	}
+
+	// public void run() {
+	// byte[] buffer = new byte[BUFFER_SIZE];
+	// fromClient = null;
+	// toClient = null;
+	//
+	// try {
+	// /**
+	// * get the input and output streams associated with the socket.
+	// */
+	// fromClient = new BufferedInputStream(client.getInputStream());
+	// toClient = new BufferedOutputStream(client.getOutputStream());
+	// int numBytes;
+	// String host;
+	//
+	// /** continually loop until the client closes the connection */
+	// while ((numBytes = fromClient.read(buffer)) != -1) {
+	// // What the client sent us.
+	// host = new String(buffer, 0, numBytes);
+	// BufferedReader bufReader = new BufferedReader(new StringReader(host));
+	//
+	// // This gets the entire message including line breaks.
+	// char[] cin = new char[2048];
+	// bufReader.read(cin);
+	//
+	// String input = String.valueOf(cin);
+	// input = input.trim();
+	//
+	// ClientCommand cc = ClientCommand.parse(input);
+	//
+	// // check if user has already logged in
+	// if (active == true && cc.command.equals("login")) {
+	// writeError("You have already logged in.");
+	// }
+	//
+	// // Log the user in.
+	// while (active == false) {
+	// if (!cc.command.equals("login") || cc.arg.isEmpty() ||
+	// cc.command.equals("")) {
+	// writeBadSyntax("Login command is '/login username \\r\\n\'");
+	// return;
+	// } else {
+	// if (users.contains(cc.arg)) {
+	// writeToClient("UsernameTaken\r\n");
+	// // Close the connection.
+	// client.close();
+	// } else if (cc.arg.length() > 16) {
+	// writeError("Username must be between 1 and 16 characters");
+	// // Close the connection.
+	// client.close();
+	// } else {
+	// uname = cc.arg;
+	// users.add(uname);
+	// active = true;
+	//
+	// writeToClient("Welcome\r\n");
+	// writeToAllClients("Connected " + uname + "\r\n");
+	// }
+	// }
+	// }
+	//
+	// // command and error handling
+	// if (cc.command.equals("public")) {
+	// // public messages
+	// if (!cc.arg.equals("\\r\\n")) {
+	// writeBadSyntax("");
+	// } else {
+	// publicToAllClients(cc.data, uname);
+	// }
+	// } else if (cc.command.equals("private")) {
+	// // private messages
+	// String args[] = cc.arg.split(" ");
+	// if (args.length > 1) {
+	// String name = args[0];
+	// String rtn = args[1];
+	// if (name.equals("\\r\\n")) {
+	// writeError("Enter a username.");
+	// } else if (!rtn.equals("\\r\\n")) {
+	// writeBadSyntax("Private command is '/private username \\r\\n mesage\'");
+	// }
+	// } else {
+	// privateToUser(cc.data, cc.arg);
+	// }
+	// } else if (cc.command.equals("users")) {
+	// // users: check for extra arguments
+	// if (cc.arg.equals("")) {
+	// writeUserList(users);
+	// } else {
+	// writeBadSyntax("/users takes no arguments");
+	// }
+	// } else if (cc.command.equals("close")) {
+	// // close: check for extra arguments
+	// if (cc.arg.equals("")) {
+	// writeToAllClients("Disconnected " + uname + "\r\n");
+	// client.close();
+	// users.remove(uname);
+	// clients.remove(this);
+	// } else {
+	// writeBadSyntax("/close takes no arguments");
+	// }
+	// } else if (cc.command.equals("login")) {
+	// // handles lack of command immediately after logging in
+	// continue;
+	// } else {
+	// // if user enters ANYTHING else, it's not a valid command!
+	// writeBadSyntax("");
+	// }
+	// }
+	// } catch (IOException ioe) {
+	// System.err.println(ioe);
+	// } finally {
+	// // close streams and socket
+	// try {
+	// if (fromClient != null)
+	// fromClient.close();
+	// if (toClient != null)
+	// toClient.close();
+	// } catch (Exception e) {}
+	// }
+	// }
+	//
+	// /**
+	// * Helper methods!
+	// */
+	//
+	// // writes the user list to client
+	// private void writeUserList(List<String> u) {
+	// String out = "ActiveUsers {";
+	// synchronized (u) {
+	// Iterator<String> i = u.iterator();
+	// while (i.hasNext()) {
+	// out = out + i.next();
+	//
+	// if (i.hasNext())
+	// out = out + ",";
+	// }
+	// }
+	// out = out + "}\r\n";
+	// writeToClient(out);
+	// }
+	//
+	// // writes a private message to a specified user
+	// private void privateToUser(String input, String dest_uname) {
+	// synchronized (clients) {
+	// Connection dst = null;
+	// for (Connection s : clients) {
+	// if (s.uname.equals(dest_uname)) {
+	// dst = s;
+	// break;
+	// }
+	// }
+	// if (dst != null && dest_uname.equals(uname)) {
+	// // user is sending private message to self: allowed, but silly
+	// String funny = "Why are you writing messages to yourself?\r\n";
+	// dst.writeToClient("Private " + uname + "\r\n" + funny + input +
+	// "\u0004\r\n");
+	// } else if (dst != null) {
+	// // main private message
+	// dst.writeToClient("Private " + uname + "\r\n" + input + "\u0004\r\n");
+	// } else {
+	// // username was not found
+	// writeError("User '" + dest_uname + "' not found");
+	// }
+	// }
+	// }
+	//
+	// // writes input to all clients, such as connected and disconnected users
+	// private void writeToAllClients(String input) {
+	// synchronized (clients) {
+	// Iterator<Connection> i = clients.iterator();
+	// while (i.hasNext()) {
+	// Connection r = i.next();
+	// if (r.active)
+	// r.writeToClient(input);
+	// }
+	// }
+	// }
+	//
+	// // writes a public messages to all clients
+	// private void publicToAllClients(String input, String sender_uname) {
+	// synchronized (clients) {
+	// Iterator<Connection> i = clients.iterator();
+	// while (i.hasNext()) {
+	// Connection r = i.next();
+	// if (r.active)
+	// r.writePublicMessage(input, sender_uname);
+	// }
+	// }
+	// }
+	//
+	// // helper method for publicToAllClients
+	// private void writePublicMessage(String msg, String sender_uname) {
+	// writeToClient("Public " + sender_uname + "\r\n" + msg + "\u0004\r\n");
+	// }
+	//
+	// // meat and potatoes - writing messages!
+	// private void writeToClient(String msg) {
+	// msg = "]" + msg;
+	// try {
+	// toClient.write(msg.getBytes("UTF-8"));
+	// toClient.flush();
+	// } catch (IOException e) {
+	// }
+	// }
+	//
+	// /**
+	// * Error message methods!
+	// * @param msg
+	// */
+	//
+	// // bad syntax: takes an input message if desired
+	// private void writeBadSyntax(String msg) {
+	// writeToClient("BadSyntax " + msg + "\r\n");
+	// }
+	//
+	// // general error: takes an input message if desired
+	// private void writeError(String msg) {
+	// writeToClient("Error " + msg + "\r\n");
+	// }
 
 }
